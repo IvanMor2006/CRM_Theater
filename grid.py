444e876948db8fb8d4281842cf4346c324fbf30c
IvanMor2006 <118119868+IvanMor2006@users.noreply.github.com>
2026-04-07 14:07:41 +0300

    новый метод select_row_by_id; спрятал поля ID

--- grid.py
+++ grid.py
@@ -13,6 +13,7 @@ class Grid:
         
         self.frame = tk.Frame(parent)
         self.frame.pack(fill='y', side='left')
+        self.frame.config(takefocus=False)
 
         self.button = tk.Button(self.frame, text='Снять выделение', command=lambda: self.element.selection_set([]))
         self.button.pack(side='bottom')
@@ -23,7 +24,7 @@ class Grid:
         scrollbar_y.pack(side='right', fill='y')
 
         self.element = ttk.Treeview(self.frame, columns=COLUMNS, show='headings', selectmode='extended' if select_many_raws else 'browse', xscrollcommand=scrollbar_x.set, yscrollcommand=scrollbar_y.set)
-
+        self.element.config(takefocus=False)
         scrollbar_x.config(command=self.element.xview)
         scrollbar_y.config(command=self.element.yview)
 
@@ -31,10 +32,21 @@ class Grid:
             self.element.insert('', 'end', values=tuple(row))
         for f, c, w in zip(self.FIELDS, COLUMNS, widths):
             n, t = f
+            if 'ID' in n:
+                w = 0
             self.element.heading(c, text=n)
-            self.element.column(c, width=w)
+            self.element.column(c, width=w, stretch=False)
         self.element.pack(expand=True, fill='both')
 
+    def select_row_by_id(self, id):
+        for item in self.element.get_children():
+            buf_id = int(self.element.item(item, 'values')[0])
+            if buf_id == id:
+                self.element.selection_set(item)
+                self.element.see(item)
+                return True
+        return False
+        
     def get_selected_rows(self):
         id = []
         rows_id = self.element.selection()
