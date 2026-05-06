89c4f1dda5dccd1a4cb52f055034017eb3c7c67a
IvanMor2006 <118119868+IvanMor2006@users.noreply.github.com>
2026-05-06 20:46:39 +0300

    Фильтрация исполнителей

--- window.py
+++ window.py
@@ -24,6 +24,16 @@ class Window:
         self.menu = Menu(self)
 
         self.grids: dict[str, Grid] = {}
+        self.buttons: dict[str, tk.Button] = {}
+
+    def check_rows(self, *args):
+        _, grid = self.get_current_grid()
+        if not grid.rows:
+            self.buttons['Изменить'].config(state=tk.DISABLED)
+            self.buttons['Удалить'].config(state=tk.DISABLED)
+        else:
+            self.buttons['Изменить'].config(state=tk.NORMAL)
+            self.buttons['Удалить'].config(state=tk.NORMAL)
 
     def start(self):
         self.window.mainloop()
@@ -32,23 +42,27 @@ class Window:
         if self.button_frame is None:
             self.button_frame = tk.Frame(self.window)
             self.button_frame.pack(fill='y', side='left', padx=10)
-
         button = tk.Button(self.button_frame, text=text, command=command)
+        self.buttons[text] = button
         return button
 
     def new_tab(self, text):
         if self.notebook is None:
             self.notebook = ttk.Notebook(self.window)
             self.notebook.pack(expand=True, fill='both')
-
+            self.notebook.bind("<<NotebookTabChanged>>", self.check_rows)
         tab = Tab(self.notebook, text)
         return tab
 
-    def row(self, action: Literal['add', 'change', 'delete']):
+    def get_current_grid(self) -> tuple[str, Grid]:
         table = self.notebook.tab('current', 'text')
+        return table, self.grids[table]
+
+    def row(self, action: Literal['add', 'change', 'delete']):
+        table, grid = self.get_current_grid()
         if action == 'delete':
             id:tuple
-            id = self.grids[table].get_selected_rows()
+            id = grid.get_selected_rows()
             if len(id) == 0:
                 messagebox.showwarning('Предупреждение', 'Не выбраны записи для удаления')
                 return
@@ -57,15 +71,16 @@ class Window:
             except Exception as e:
                 messagebox.showerror('Ошибка', 'Нельзя удалить запись: она используется в других таблицах!')
                 return
-            self.grids[table].update()
+            grid.update()
+            self.check_rows()
         elif action == 'add' or action == 'change':
             if action == 'change':
-                id = self.grids[table].get_selected_rows()
+                id = grid.get_selected_rows()
                 if len(id) != 1:
                     messagebox.showwarning('Предупреждение', 'Не выбрана запись для изменения' if len(id) == 0 else 'Выберите одну запись')
                     return
                 _, rows = db.select(f'SELECT * FROM {table} WHERE ID = {id[0]}')
-            fields = self.grids[table].FIELDS
+            fields = grid.FIELDS
             new_fields = []
             for i in range(1, len(fields)):
                 n, t = fields[i]
@@ -101,21 +116,51 @@ def __main__():
     for title, info in config.TABLES.items():
         if title == 'Режиссёр':
             continue
+
         tab = window.new_tab(title)
         window.grids[title] = tab.new_grid(info['query'])
+
         def log():
             query = '''
                 SELECT *
-                FROM БилетLog
-                ORDER BY datelog DESC, ID DESC
+                  FROM БилетLog
+                  ORDER BY datelog DESC, ID DESC
             '''
             log_window = ChildWindow(window.window, 'Log Билетов')
             log_window.element.focus()
-            grid = Grid(log_window.element, query, False)
-            grid.frame.pack(padx=10, pady=10, side='top', fill='both', expand=True)
+            check_frame = tk.Frame(log_window.element)
+            check_frame.del_val = tk.BooleanVar()
+            def filt():
+                conditions = []
+                if check_frame.del_val.get():
+                    conditions.append("typelog = 'D'")
+                if check_frame.ins_val.get():
+                    conditions.append("typelog = 'I'")
+                where = f'WHERE {" OR ".join(conditions)}' if conditions else ''
+                log_window.grid.QUERY = f'''
+                    SELECT *
+                      FROM БилетLog
+                      {where}
+                      ORDER BY datelog DESC, ID DESC
+                '''
+                if conditions:
+                    log_window.grid.update()
+                else:
+                    log_window.grid.clear()
+            deleted_cb = tk.Checkbutton(check_frame, text='Удалённые', variable=check_frame.del_val, command=filt)
+            deleted_cb.pack(side='left', expand=True)
+            check_frame.del_val.set(True)
+            check_frame.ins_val = tk.BooleanVar()
+            inserted_cb = tk.Checkbutton(check_frame, text='Добавленные', variable=check_frame.ins_val, command=filt)
+            inserted_cb.pack(side='left', expand=True)
+            check_frame.ins_val.set(True)
+            check_frame.pack()
+            log_window.grid = Grid(log_window.element, query, False)
+            log_window.grid.frame.pack(padx=10, pady=10, side='top', fill='both', expand=True)
         if title == 'Билет':
             tk.Button(window.grids[title].button_frame, text='Log', command=log).pack(side='left', expand=True, anchor='w', padx=20)
             window.grids[title].button.pack(anchor='e')
+
         def delete_performances():
             grid: Grid = window.grids['Представление']
             rows = grid.get_selected_rows()
@@ -126,8 +171,28 @@ def __main__():
                 db.query(f'EXEC ОтменаПредставления {id}')
                 window.grids['Билет'].update()
                 grid.update()
+            window.check_rows()
         if title == 'Представление':
             tk.Button(window.grids[title].button_frame, text='Отменить представления', command=delete_performances).pack(side='left', expand=True, anchor='w', padx=20)
+            window.grids[title].button.pack(anchor='e')
+
+        def select_performers():
+            select_window = ChildWindow(window.window, 'Выберите спектакль', '300x75')
+            _, data = db.select('SELECT Название FROM Спектакль')
+            select_play = ttk.Combobox(select_window.element, values=[''] + [row[0] for row in data], width=40, state='readonly')
+            select_play.pack(pady=10)
+            def filt(play):
+                if not play:
+                    play == 'NULL'
+                grid = window.grids['Исполнитель']
+                grid.QUERY = f'''
+                    SELECT * FROM dbo.ИсполнителиСпектакля((SELECT ID FROM Спектакль WHERE Название = '{play}'))
+                '''
+                grid.update()
+                window.check_rows()
+            tk.Button(select_window.element, text='Отфильтровать по спектаклю', command=lambda: filt(select_play.get())).pack()
+        if title == 'Исполнитель':
+            tk.Button(window.grids[title].button_frame, text='Выбрать конкретных исполнителей', command=select_performers).pack(side='left', expand=True, anchor='w', padx=20)
             window.grids[title].button.pack(anchor='e')
     window.start()
 
